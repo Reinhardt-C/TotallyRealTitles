c7007a941fe9b65e1bc1d8bcd03838db889d14b0
Logan M <>
2024-05-01 19:10:08 +1200

    more titles

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,30 @@
 const TEMPVISTITLES = {
-    "fluoxetine_eater": ["Cutie Master", "CM"],
-    "ReinhardtC": ["Not Master", "NM"]
+    "fluoxetine_eater": ["Cutie Master", "CM", false],
+    "ReinhardtC": ["Not Master", "NM", false],
+    "alireza2003": ["Lose Master", "LM", true],
+    "german11": ["Super Grandmaster", "SGM", true],
+    "KiwiCoach": ["Horde Master", "HM", true]
 };
 
 function run(muts) {
     const userlinks = [...(muts ? muts[0].target : document).querySelectorAll(":is(a, span):is(.ulink,.user-link):not(:has(> .utitle))")];
     for (let l of userlinks) for (let u in TEMPVISTITLES) 
         if (l.innerHTML.search(u) > -1) 
-            addTitle(l, ...TEMPVISTITLES[u]);
+            addTitle(l, ...TEMPVISTITLES[u].slice(0,2));
+    const userlinkst = [...(muts ? muts[0].target : document).querySelectorAll(":is(a, span):is(.ulink,.user-link):has(> .utitle)")];
+    for (let l of userlinkst) for (let u in TEMPVISTITLES) 
+        if (TEMPVISTITLES[u][2] && l.innerHTML.search(u) > -1) {
+            l.querySelector(".utitle").remove();
+            addTitle(l, ...TEMPVISTITLES[u].slice(0,2), true);
+        }
 }
 
-function addTitle(l, name = "Master", abbr = "M", colour = "#d6483e") {
+function addTitle(l, name = "Master", abbr = "M", repl = false, colour = "#d6483e") {
     const title = document.createElement("span");
     title.className = "utitle"; title.title = name;
     title.innerText = abbr; title.style.color = colour;
     const childtext = [...l.childNodes].filter(e => e.nodeValue)[0];
-    console.log(l, childtext);
-    childtext.nodeValue = "\u00A0" + childtext.nodeValue;
+    childtext.nodeValue = (repl ? "" : "\u00A0") + childtext.nodeValue;
     l.insertBefore(title, childtext);
 
 }
